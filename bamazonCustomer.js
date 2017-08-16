03270f6ca0b0de6b9bbdece1eb412676898d8407
keisha <keisha@sample.com>
2017-08-16 12:10:09 -0400

    working on code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,58 +1,101 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+var prompt = require("prompt");
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     port: 3306,
     database: "bamazon"})
+
 connection.connect(function (err){
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    showAll();
+    console.log("WELCOME TO BAMAZON!"); 
+    runSearch();   
 });
 
-function showAll(){
-connection.query('SELECT * FROM products', function(err, res){
-        if (err) throw err;
+
+var runSearch = function() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+    }).then(function(answer) {
+        console.log(answer);
+        switch(answer.action.toLowerCase()) {
+            case 'view products for sale':
+                viewProducts();
+            break;
+            
+            case 'view low inventory':
+                viewLowInventory();
+            break;
+            
+            case 'add to inventory':
+                addToInventory();
+            break;
+            
+            case 'add new product':
+                addNewProduct();
+            break;
+        }
+    })
+};
+
+
+function viewProducts(){
+    connection.query("SELECT * FROM Bamazon.Products", function(err, res) {
+        if(err) throw err;
         console.log(res);
-        });
+    });
 }
 
-function start() {
-  inquirer    
-    .prompt([
-        {
-            type:'input',
-            name:'ID'
-            message:'Enter the Item ID for the item you want to purchase?'
-            validate: function(value) {
-                if (isNaN(value) === false && value <= 10) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }    
-        },{
-            type:'input',
-            name:'quantity'
-            message:'How many do you want to purchase?'
-            validate: function(value) {
-                if (isNaN(value)) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        }
+function viewLowInventory(){
+    connection.query("SELECT * FROM Bamazon.Products where StockQuantity < 100;", function(err, res) {
+            if(err) throw err;
+            console.log(res);
+        });
+    }
 
-//         }],function(err,res){
-//             if (err){
-//                 console.log('prompt error');
-//             };
-//         });
-// }
-start();
+function addToInventory(){
+    inquirer.prompt([{
+        name: "product",
+        type: "input",
+        message: "itemId to add to"
+    },{
+        name: "quantity",
+        type: "input",
+        message: "quantity to add"
+    }]).then(function(answer) {
+        connection.query("UPDATE products SET StockQuantity = StockQuantity + " + answer.quantity + " WHERE itemId = " + answer.product + ";", function(err, res) {
+            if(err) throw err;
+            console.log(res);
+        });
+    })
+}
 
+function addNewProduct(){
+    inquirer.prompt([{
+        name: "product",
+        type: "input",
+        message: "product to add to inventory"
+    },{
+        name: "departmentName",
+        type: "input",
+        message: "department"
+    },{
+        name: "price",
+        type: "input",
+        message: "price"
+    },{
+        name: "quantity",
+        type: "input",
+        message: "quantity to add"
+    }]).then(function(answer) {
+        connection.query("INSERT INTO `Bamazon`.`products` (`ProductName`, `DepartmentName`, `Price`, `StockQuantity`) VALUES ('" + answer.product + "', '" + answer.department + "', '" + answer.price + "', '" + answer.quantity + "');", function(err, res) {
+            if(err) throw err;
+            console.log(res);
+        });
+    })
+}
